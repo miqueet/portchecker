58a736e96d6b840dbf336d645e8d6cf725fee372
Wheaties466 <mwheat42289@gmail.com>
2016-11-17 22:17:01 +0000

    modified:   portopen.py

--- portopen.py
+++ portopen.py
@@ -2,11 +2,21 @@
 #python portopen.py 1.1.1.1 80
 import socket;
 import sys
-ip=sys.argv[1]
-port=int(sys.argv[2])
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-result = sock.connect_ex((ip,port))
-if result == 0:
-   print "Port is open"
-else:
-   print "Port is CLOSED"
+import errno
+import signal
+import time
+import timeout_decorator
+
+@timeout_decorator.timeout(2)
+def ptest():
+    ip=sys.argv[1]
+    port=int(sys.argv[2])
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex((ip,port))
+    if result == 0:
+       print "Port is open"
+    else:
+       print "Port is CLOSED"
+
+if __name__ == '__main__':
+    ptest()
